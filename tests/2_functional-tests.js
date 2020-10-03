9dfcf61de52cd55ec3d080865010013a7f51d07c
Glitch (closed-smiling-citron) <none>
2020-10-03 18:44:37 +0000

    💧🐽 Updated with Glitch

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,3 +1,4 @@
+
 /*
 *
 *
@@ -13,12 +14,15 @@ var server = require('../server');
 
 chai.use(chaiHttp);
 
+let id = ''
+
 suite('Functional Tests', function() {
 
   /*
   * ----[EXAMPLE TEST]----
   * Each test should completely test the response of the API end-point including response status code!
   */
+	/*
   test('#example Test GET /api/books', function(done){
      chai.request(server)
       .get('/api/books')
@@ -37,46 +41,99 @@ suite('Functional Tests', function() {
 
   suite('Routing tests', function() {
 
-
     suite('POST /api/books with title => create book object/expect book object', function() {
       
       test('Test POST /api/books with title', function(done) {
-        //done();
+         chai.request(server)
+          .post('/api/books')
+          .send({
+            title: 'Crime and Punishment'
+          })
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.equal(res.body.title, 'Crime and Punishment')
+            assert.isNotNull(res.body._id)
+            id = res.body._id;
+            console.log("id has been set as " + id)
+            done();
+          });
       });
       
       test('Test POST /api/books with no title given', function(done) {
-        //done();
+        chai.request(server)
+          .post('/api/books')
+          .send({})
+          .end(function(err, res){
+            assert.equal(res.body, 'missing title')
+            done();
+          });
       });
       
     });
-
 
     suite('GET /api/books => array of books', function(){
       
       test('Test GET /api/books',  function(done){
-        //done();
+        chai.request(server)
+          .get('/api/books')
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.isArray(res.body, 'response should be an array');
+            assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
+            assert.property(res.body[0], 'title', 'Books in array should contain title');
+            assert.property(res.body[0], '_id', 'Books in array should contain _id');
+            done();
+          });
       });      
       
     });
 
-
     suite('GET /api/books/[id] => book object with [id]', function(){
       
       test('Test GET /api/books/[id] with id not in db',  function(done){
-        //done();
+        chai.request(server)
+          .get('/api/books/' + 'idthatdoesntexist')
+          .end(function(err, res){
+            assert.equal(res.body, 'no book exists')
+						done()
+          });
       });
       
       test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
+        chai.request(server)
+          .get('/api/books/' + id)
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.equal(res.body._id, id)
+            assert.equal(res.body.title, 'Crime and Punishment')
+						done()
+          });
       });
       
     });
 
-
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
       test('Test POST /api/books/[id] with comment', function(done){
-        //done();
+        chai.request(server)
+          .post('/api/books/' + id)
+          .send({
+            comment: 'test comment'
+          })
+          .end(function(err, res){
+
+            assert.isTrue(res.body.comments.includes('test comment'))
+
+						chai
+						.request(server)
+						.delete("/api/books/" + id)
+						.send({})
+						.end(function(err, res) {
+							done();
+						});
+          });
+
+				
       });
       
     });
